0a2b900326687276bd682585a5e716898a5ba374
youssefhe5 <55581827+youssefhe5@users.noreply.github.com>
2020-04-27 14:56:29 -0400

    Added Main Project

---
+++ OneShotGame/Assets/Ball.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    Vector2 touchStartingPosition;
+    Vector2 touchEndingPosition;
+    private Rigidbody2D rb;
+    bool canMove = false;
+    public Vector2 startingPosition = new Vector2(-0.7793833f, -1.404789f);
+    public int force = 2;
+    public int frameRate = 60;
+    private Touch touch;
+
+
+    void Start()
+    {
+        canMove = true;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, 0);
+        rb.isKinematic = true;
+        rb.position = startingPosition;
+        //Application.targetFrameRate = frameRate;
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchStartingPosition = Input.GetTouch(0).position;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                if (canMove)
+                {
+                    touchEndingPosition = Input.GetTouch(0).position;
+                    rb.isKinematic = false;
+                    rb.AddForce(force * (touchStartingPosition - touchEndingPosition));
+                }
+            }
+
+            if (touch.tapCount == 2)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+    }
+      void OnMouseUp()
+    {
+        if (canMove)
+        {
+            touchEndingPosition = Input.mousePosition;
+            rb.isKinematic = false;
+            rb.AddForce(force * (touchStartingPosition - touchEndingPosition));
+        }
+        
+                    
+
+    }
+
+    void OnMouseDown()
+    {
+        touchStartingPosition = Input.mousePosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Bounce boy bounce!");
+        }
+      
+    }
+
+}
---
+++ OneShotGame/Assets/Goal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.StartsWith("Ball"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
